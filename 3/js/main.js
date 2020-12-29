6bb7ce3be3d8886b2c1becbc96353cf5bccbe441
Rillai <62833808+Rillai@users.noreply.github.com>
2020-12-29 16:00:21 +0400

    Lesson2,3,4

---
+++ 3/js/main.js
@@ -0,0 +1,126 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+class ProductsList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this._getProducts()
+            .then(data => {
+                this.goods = [...data];
+                this.render()
+            });
+    }
+    _getProducts() {
+        return fetch(`${API}/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
+    }
+    calcSum() {
+        return this.allProducts.reduce((accum, item) => accum += item.price, 0);
+    }
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const productObj = new ProductItem(product);
+            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend', productObj.render());
+        }
+
+    }
+}
+
+
+class ProductItem {
+    constructor(product, img = 'https://placehold.it/200x150') {
+        this.title = product.product_name;
+        this.price = product.price;
+        this.id = product.id_product;
+        this.img = img;
+    }
+    render() {
+        return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <div class="desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} $</p>
+                    <button class="buy-btn">Купить</button>
+                </div>
+            </div>`
+    }
+}
+
+class Cart {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+    }
+    _getProducts() {
+        return fetch(`${API}/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
+    }
+    removeProduct(element) {
+        this.getJson(`${API}/deleteFromBasket.json`)
+            .then(data => {
+                if (data.result === 1) {
+                    let productId = +element.dataset['id'];
+                    let item = this.allProducts.find(product => product.id_product === productId);
+                    if (item.quantity > 1) {
+                        item.quantity--;
+                        this._updateCart(item);
+                    } else {
+                        this.allProducts.splice(this.allProducts.indexOf(item), 1);
+                        document.querySelector(`.cart-item[data-id="${productId}"]`).remove();
+                    }
+                } else {
+                    alert('Error');
+                }
+            })
+    }
+    _updateCart(product) {
+        let block = document.querySelector(`.cart-item[data-id="${product.id_product}"]`);
+        block.querySelector('.product-quantity').textContent = `Quantity: ${product.quantity}`;
+        block.querySelector('.product-price').textContent = `$${product.quantity * product.price}`;
+    }
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const productObj = new ProductItem(product);
+            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend', productObj.render());
+        }
+    }
+}
+class CartItem {
+    constructor(item, img = 'https://placehold.it/50x100') {
+        this.name = item.name;
+        this.price = item.price;
+        this.id = item.id;
+        this.quantity=item.quantity
+        this.img = img;
+    }
+    render() {
+        return `<div class="cart-item" data-id="${this.id}">
+            <div class="product-bio">
+            <img src="${this.img}" alt="Some image">
+            <div class="product-desc">
+            <p class="product-title">${this.name}</p>
+            <p class="product-quantity">Quantity: ${this.quantity}</p>
+        <p class="product-single-price">$${this.price} each</p>
+        </div>
+        </div>
+        <div class="right-block">
+            <p class="product-price">$${this.quantity * this.price}</p>
+            <button class="del-btn" data-id="${this.id}">&times;</button>
+        </div>
+        </div>`
+    }
+}
+let cart = new Cart();
+let list = new ProductsList();
